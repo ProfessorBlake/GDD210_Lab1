44a223e9e86dfc22384d7e8a541b43137e70c708
Blake <telefragent@gmail.com>
2019-08-24 07:36:00 -0400

    Added UsingDebugLog scene
    Simple tutorial explaining variables and the start function, as well as two types of logging.

---
+++ Assets/Game/Scripts/Debug Manager/DebugManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class DebugManager : MonoBehaviour
+{
+	//Public variables are accessible anywhere inside this class, and by other classes.
+	public string MyName = "Student";
+	public int Age = 20;
+
+	//Private variables are only accessible inside this class. Adding [serializedfield] attribute allows us to edit them in the inspector.
+	[SerializeField] private float gpa = 3.8f;
+
+	//Start is a function inhereted from MonoBehaviour, which is called at the start of the game.
+	void Start()
+    {
+		Debug.Log("My name is " + MyName + ". I am " + Age + " and my GPA is " + gpa); //Normal Debug.log
+
+		Debug.LogFormat("My name is {0}. I am {1} and my GPA is {2}", MyName, Age, gpa); //Using Debug.LogFormat
+    }
+}
