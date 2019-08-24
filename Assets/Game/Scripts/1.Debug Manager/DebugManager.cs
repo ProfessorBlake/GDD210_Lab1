e73a6758e2561739d88eecf0b2e2e9ac1d765ce7
Blake <telefragent@gmail.com>
2019-08-24 07:57:25 -0400

    Added array tutorial

--- Assets/Game/Scripts/1.Debug Manager/DebugManager.cs
+++ Assets/Game/Scripts/1.Debug Manager/DebugManager.cs
@@ -8,6 +8,7 @@ public class DebugManager : MonoBehaviour
 
 	//Private variables are only accessible inside this class. Adding [serializedfield] attribute allows us to edit them in the inspector.
 	[SerializeField] private float gpa = 3.8f;
+	[SerializeField] private bool iHaveACar = true;
 
 	//Start is a function inhereted from MonoBehaviour, which is called at the start of the game.
 	void Start()
@@ -15,5 +16,11 @@ public class DebugManager : MonoBehaviour
 		Debug.Log("My name is " + MyName + ". I am " + Age + " and my GPA is " + gpa); //Normal Debug.log
 
 		Debug.LogFormat("My name is {0}. I am {1} and my GPA is {2}", MyName, Age, gpa); //Using Debug.LogFormat
+
+		//Using an if statement.
+		if (iHaveACar == true)
+		{
+			Debug.Log("I have a car!");
+		}
     }
 }
---
+++ Assets/Game/Scripts/2.Arrays/ArrayManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class ArrayManager : MonoBehaviour
+{
+	//Array of 3 strings.
+	private string[] Friends = new string[3];
+
+	private void Start()
+	{
+		//Set our friends names.
+		Friends[0] = "Alice"; //The first index of an array is 0.
+		Friends[1] = "Bob";
+		Friends[2] = "Charlie";
+
+		//Our for loop will create a temporary variable 'i' and set it to 0.
+		//While i is less than the length of Friends (3) it will go through our loop.
+		//After each loop, it will add 1 to i and check the condition again.
+		for(int i = 0; i < Friends.Length; i++)
+		{
+			Debug.Log(Friends[i]); //Log the item in Friends at index i.
+		}
+
+		//A foreach loop will look for all the strings in 'Friends' and assign them to 'name' inside the loop.
+		foreach(string name in Friends)
+		{
+			Debug.Log(name);
+		}
+	}
+}
